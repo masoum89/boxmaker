71112ae2d22143047c606ef72506a406a0eeee5c
jhm9318 <jhm9318@gmail.com>
2013-05-19 18:42:14 +0430

    fix some lines

--- fix_text.py
+++ fix_text.py
@@ -12,20 +12,23 @@ def fix():
         for word in line.split():
             if( calc_length(word) < screen ):
                 outFile.write(word)
-            else
+            else:
                 outFile.write('\n'+word)              
 
 
 def calc_length(word):
     length = 0
     for ch in word:
-        length = length + xList[ch][2]
+        length = length + lengthList[ch][2]
         
     return length
 
 
 def main():
-    pass
+    if len(sys.argv) != 3:
+        sys.exit( "file %s not found" % sys.argv[0] )
+    else:
+        fix()
     
         
 if __name__ == '__main__':
