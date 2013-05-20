03a174255c4318d52a7af271289ec4c0532e8357
jhm9318 <jhm9318@gmail.com>
2013-05-20 12:24:47 +0430

    add some flow control in fix method

--- fix_text.py
+++ fix_text.py
@@ -2,34 +2,38 @@
 
 import codecs
 import sys
+import bound
+
+length = 0
 
 def fix():
     
-    inFile = codecs.open(sys.argv[1], "r")
-    outFile = codes.open("out.txt", "w")
+    inFile = codecs.open(sys.argv[1], "r","utf-8")
+    outFile = codecs.open("out.txt", "w","utf-8")
+    global length
+    temp=0
     
     for line in inFile:
         for word in line.split():
-            if( calc_length(word) < screen ):
-                outFile.write(word)
+            if( calc_length(word) < 1300 ):
+                outFile.write(word+' ')
+                temp=length
+
             else:
-                outFile.write('\n'+word)              
+                outFile.write('\n'+ word +' ')
+                length=length-temp			
 
 
 def calc_length(word):
-    length = 0
-    for ch in word:
-        length = length + lengthList[ch][2]
-        
-    return length
+
+	for ch in word:
+		global length
+		length = length + bound.lengthList[ch][2]
+	return length
 
 
 def main():
-    if len(sys.argv) != 3:
-        sys.exit( "file %s not found" % sys.argv[0] )
-    else:
-        fix()
-    
+	fix()
         
 if __name__ == '__main__':
     main()
